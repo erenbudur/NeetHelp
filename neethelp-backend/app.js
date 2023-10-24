09d0e5060c1aeb0db902787eb5cdc2e37e29c3ee
erenbudur <56324781+erenbudur@users.noreply.github.com>
2023-10-24 17:36:38 -0400

    Change routing for deployment

--- neethelp-backend/app.js
+++ neethelp-backend/app.js
@@ -1,4 +1,4 @@
-var createError = require("http-errors");
+
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
@@ -15,26 +15,8 @@ app.set("view engine", "pug");
 
 
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/video", videoRouter);
-
-// catch 404 and forward to error handler
-
-// error handler
-app.use(function (err, req, res) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
 
 
 module.exports = app;
